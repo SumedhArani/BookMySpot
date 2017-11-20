5157c83930fa2cfb55f71a395bd31226b9a1f069
Sumedh Arani <aranisumedh@gmail.com>
2017-11-20 09:53:06 +0530

    Successful deletion and added PHP code for client validation(untested)

--- client_delete.php
+++ client_delete.php
@@ -1,20 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <script>
+  <?php
 
-include 'connect_to_db.php';
-	extract($_GET);
+    include 'connect_to_db.php';
+    extract($_GET);
 
-   	$query = "select client_id from client WHERE username = "."'".$uname."'"." AND passwd ="."'".$pword."'";
-   	$result = mysqli_query($con , $query);
+    $query = "select client_id from client WHERE username = "."'".$uname."'"." AND passwd ="."'".$pword."'";
+    $result = mysqli_query($con , $query);
 
-   	while($client = mysqli_fetch_array($result , MYSQLI_ASSOC))
-   	{
-   		$client_id = $client['client_id'];
-   		$sql = "DELETE FROM client WHERE client_id = "."'".$client_id."'";
-   		if (mysqli_query($con, $sql)){
-    		echo "Record deleted successfully";
-		} else {
-    		echo "Error deleting record: " . $con->error;
-		}
-   	}
-
-?>	
+    while($client = mysqli_fetch_array($result , MYSQLI_ASSOC))
+    {
+      $client_id = $client['client_id'];
+      $sql = "DELETE FROM client WHERE client_id = "."'".$client_id."'";
+      if (mysqli_query($con, $sql)){
+        echo "parent.showDiv("."'"."Mall deleted successfully"."'".")";
+    } else {
+        echo "parent.showDiv("."'"."We didn't find you! :("."'".")";
+    }
+    }
+  ?> 
+  </script> 
+</head>
+<body>
+</body>
+</html>
